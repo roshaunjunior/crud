fc311e559fb5d5b7abb0ea2f495d648ba4a82077
Talha Roshaun <talharoshaun@gmail.com>
2022-12-27 16:44:09 +0500

    excuse generator

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
 import './App.css';
 import { useState } from 'react';
-import {Task} from './Task' ;
+import Axios from 'axios' ;
 function App() {
+  const [showExcuse,setShowExcuse] = useState('') ;
+
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(res=>setShowExcuse(res.data[0].excuse))
+  }
+    
   
-  const [todoList, setTodoList] = useState([]) ;
-  const [newTask , setNewTask] = useState('') ; 
 
-  const handleChange = (event) => {
-     setNewTask(event.target.value) ;
-  }
-  
-  const addTask = () => {
-    const taskObject = {
-      id : todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1 ,
-      taskName : newTask,
-      status : false,
-    }
-
-    setTodoList([...todoList , taskObject])
-  }
-
-  const deleteTask = (id) => {
-       setTodoList(todoList.filter((task) => task.id !== id )) ;
-  }
-
-  const completeTask = (id) => {
-     setTodoList(
-     todoList.map((task) => {
-      if(task.id === id) {
-        return {...task , status : true}
-      } else {
-        return task ;
-      }
-      
-     }))
-  }
   return (
     <div className="App">
-        <div className='addTask'>
-           <input onChange={handleChange}/>
-           <button onClick={addTask}>Add Task</button>
-        </div>
-        <div className='list'>
-          {todoList.map((task) => {
-           return <Task 
-           taskName = {task.taskName}
-           id = {task.id} 
-           deleteTask = {deleteTask}
-           completeTask = {completeTask}
-           status = {task.status}
-           />
-          })}
-        </div>
+      <h1>Excuse genrator</h1>   
+      <button onClick={()=> fetchExcuse('party')}>Party</button>
+      <button onClick={()=> fetchExcuse('family')}>Family</button>
+      <button onClick={()=> fetchExcuse('office')}>Office</button>
+          <p>{showExcuse}</p> 
     </div>
   );
 }
